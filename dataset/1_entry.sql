e718fa5c24c29aab2add76996094efc94af2fa59
Torsten Chandler <torstenchandler@gmail.com>
2024-06-13 17:03:03 +0200

    SQL tables added

--- dataset/1_entry.sql
+++
@@ -1,5 +0,0 @@
-create table entry (
-    id uuid primary key default gen_random_uuid(),
-    text text not null,
-    date date default now()
-);
--- dataset/2_entry_data.sql
+++
@@ -1,3 +0,0 @@
-insert into "entry" (text,date) values
-('random entry of the day ... ', <user-id>, ,'2024-06-11'),
-('random entry of the day ... ', '2024-06-11');
