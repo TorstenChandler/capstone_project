751ae1f76b6e0238b15374b55b560f589e6a648a
Olmo <Olmo@MacBook-Pro-2.local>
2024-06-14 11:36:55 +0200

    updating gitingore

--- actions/app/main.py
+++ actions/app/main.py
@@ -27,12 +27,27 @@ class Entry(BaseModel):
     date: str
 
 
+@app.post("/new_entry")
+async def new_entry(entry:Entry):
+    #@TODO SAVE ENTRY TO DATABASE
+    return JSONResponse(jsonable_encoder({"message": "SAD TO HEAR..."}))
+
+
+
+@app.post("/ask")
+async def ask(request:Request, question:str):
+    return JSONResponse(jsonable_encoder({"answer": ""}))
+
+
+
 @app.post("/entry_inserted")
 async def entry_inserted(entry:Entry):
     #@TODOS: CALCULATE EMOTIONS AND TOPICS AND INSERT IN TABLES
     #emotionsModel = pipeline("text-classification", model="cardiffnlp/twitter-roberta-base-emotion-multilabel-latest", return_all_scores=True)
     #detectedEmotions =  emotionsModel(entry.text)[0]
     #emotions =  tuple(item['value'] for item in detectedEmotions)
+    topics = ()
+    vectors = []
     emotions = (entry.id,0.1,0.2, 0.3,0.4, 0.5,0.6, 0.7,0.8, 0.9,0.10, 0.11)
     time.sleep(1)
     with psycopg.connect(get_conn_str()) as conn:
