20b10c6a18e3e385c9222b1afd9ba8101f081174
MonaliTalele <monali.talele@gmail.com>
2024-06-18 13:05:45 +0200

    Added the llama, lanchain implementation for responding to a journal entry.

--- actions/app/rag_model.py
+++ actions/app/rag_model.py
@@ -5,6 +5,9 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_postgres import PGVector
 from langchain_postgres.vectorstores import DistanceStrategy
 from langchain_community.llms import Ollama
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
 from main import get_conn_str
 import psycopg
 import ast
@@ -14,6 +17,44 @@ import os
 
 default_model_name="llama3"
 
+def generate_entry_response(text):
+    llm = init_ollama("llama3")
+
+    #Start a question answer chat prompt
+    system_prompt = (
+        """You are an emotionally intelligent assistant. You collect journal entries from users and respond in a empathetic way. 
+        You do not ask questions, unless necessary. You reply in 2-3 sentences. """
+    )
+
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            ("human", "{input}"),
+        ]
+    )
+
+    chain = prompt_template | llm 
+
+    ### Statefully manage chat history ###
+    store = {}
+
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+
+    runnable_with_history = RunnableWithMessageHistory(
+        chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="history",
+    )
+    output = runnable_with_history.invoke({"input": text},
+        config={"configurable": {"session_id": "2"}})
+    print("Journal Entry : ", text)
+    print("Response : ", output) 
+
+    return output
 
 def ask_rag(user_id, input):
     vectorstore = populate_vector_table(user_id)
@@ -83,4 +124,8 @@ def init_rag_chain(retriever, model_name=default_model_name):
     question_answer_chain = create_stuff_documents_chain(llm, prompt)
     rag_chain = create_retrieval_chain(retriever, question_answer_chain)
     return rag_chain
+
+if __name__ == "__main__":
+    pass
+
 
